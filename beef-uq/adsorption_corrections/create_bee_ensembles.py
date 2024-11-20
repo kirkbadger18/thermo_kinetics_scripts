e7cab9898a8a38083e1b912b2b148a0e7dd02039
Bjarne Kreitz <bjarne_kreitz@brown.edu>
2024-11-20 17:19:40 -0500

    clean up adsorption corrections

--- beef-uq/adsorption_corrections/create_bee_ensembles.py
+++ beef-uq/adsorption_corrections/create_bee_ensembles.py
@@ -7,7 +7,6 @@ import glob
 class Molecule:
 
     def __init__(self):
-        # start by defining some physical constants
         self.n_bee = 2000
 
 
@@ -18,32 +17,6 @@ def parse_input(species, molecule):
     molecule.perturbation = data.iloc[:, 3].to_numpy()
 
     return molecule.perturbation
-
-# name = 'CXRXCX'
-# list_of_species = 'nodes/species_list_' + name + '.dat'
-# info = open(list_of_species, 'r')
-# species_list = info.readlines()
-# info.close()
-#
-# array_list = []
-# counter = -1
-# for species in species_list:
-#     counter += 1
-#     filename = species.strip()
-#     print(filename)
-#
-#     test = Molecule()
-#     array_list.append(parse_input(filename, test))
-#
-# stacked = np.stack(array_list, axis=0)
-# avg = np.mean(stacked, axis=0)
-#
-# output_file = 'beef-ensembles/' + name + '_bee.txt'
-# column_names = ['averaged_values']
-# df = pd.DataFrame(avg, columns=[column_names])
-# df.to_csv(output_file, sep="\t", index=False)
-
-#list_of_species = 'nodes/species_list_' + name + '.dat'
 
 for filename in glob.iglob('nodes/*.dat'):
 
