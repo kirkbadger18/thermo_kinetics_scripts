bcab5d137dc2481f976495f6385f60ab925aafa9
Kirk Badger <kirk_badger@brown.edu>
2025-01-16 17:43:11 -0500

    updated create_bee script

--- beef-uq/adsorption_corrections/create_bee_ensembles.py
+++ beef-uq/adsorption_corrections/create_bee_ensembles.py
@@ -1,45 +1,38 @@
 import numpy as np
-import os
+from os import path
 import pandas as pd
-import glob
+from tree import RX
 
 
-class Molecule:
-
-    def __init__(self):
-        self.n_bee = 2000
 
 
-def parse_input(species, molecule):
+def parse_input(species):
     script_dir = '../thermo/beef-ensembles/'
-    inputfile = str(script_dir) + str(species) + '_ads_bee.txt'
-    data = pd.read_csv(inputfile, sep="\t", header=0)
-    molecule.perturbation = data.iloc[:, 1].to_numpy()
+    inputfile = str(script_dir) + str(species) + '_bee.txt'
+    fpath = path.abspath(inputfile)
+    print(fpath)
+    data = pd.read_csv(fpath, sep="\t", header=0)
+    perturbation = data.iloc[:, 1].to_numpy()
+    return perturbation
 
-    return molecule.perturbation
+def add_backslashes(string):
+    new_str = string.replace('(','\(')
+    return new_str
 
-for filename in glob.iglob('nodes/*.dat'):
-
-    name=filename.replace('.dat','').split('_')[-1]
-    print(name)
-
-    info = open(filename, 'r')
-    species_list = info.readlines()
-    info.close()
-
-    array_list = []
-    counter = -1
+all_children = RX.get_all_children()
+all_children.append(RX)
+for child in all_children:
+    species_list = child.get_all_species()
+    array_list = []     
     for species in species_list:
-        counter += 1
-        filename = species.strip()
-
-        test = Molecule()
-        array_list.append(parse_input(filename, test))
+        filename = species
+        print(species)
+        array_list.append(parse_input(filename))
 
     stacked = np.stack(array_list, axis=0)
     avg = np.mean(stacked, axis=0)
 
-    output_file = 'beef-ensembles/' + name + '_bee.txt'
+    output_file = 'beef-ensembles/' + str(child) + '_bee.txt'
     column_names = ['averaged_values']
     df = pd.DataFrame(avg, columns=[column_names])
     df.to_csv(output_file, sep="\t", index=False)
