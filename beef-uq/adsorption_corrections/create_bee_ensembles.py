26d4f057a09cf27e8a9ce05f5391f08317e322dc
Kirk Badger <kirk_badger@brown.edu>
2025-01-17 13:51:42 -0500

    rewrote uq_ads script

--- beef-uq/adsorption_corrections/create_bee_ensembles.py
+++ beef-uq/adsorption_corrections/create_bee_ensembles.py
@@ -10,23 +10,16 @@ def parse_input(species):
     script_dir = '../thermo/beef-ensembles/'
     inputfile = str(script_dir) + str(species) + '_bee.txt'
     fpath = path.abspath(inputfile)
-    print(fpath)
     data = pd.read_csv(fpath, sep="\t", header=0)
     perturbation = data.iloc[:, 1].to_numpy()
     return perturbation
 
-def add_backslashes(string):
-    new_str = string.replace('(','\(')
-    return new_str
-
 all_children = RX.get_all_children()
-all_children.append(RX)
 for child in all_children:
     species_list = child.get_all_species()
     array_list = []     
     for species in species_list:
         filename = species
-        print(species)
         array_list.append(parse_input(filename))
 
     stacked = np.stack(array_list, axis=0)
@@ -36,4 +29,3 @@ for child in all_children:
     column_names = ['averaged_values']
     df = pd.DataFrame(avg, columns=[column_names])
     df.to_csv(output_file, sep="\t", index=False)
-
